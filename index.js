4968c8ef486cb32d9b81ad1f2514d17bab82860f
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-01-29 09:35:18 +0100

    finished system to remove color codes

--- index.js
+++ index.js
@@ -15,16 +15,24 @@ for(var i in fileObjs){
     var json = JSON.parse(rawdata);
     if(!json.hasOwnProperty("info") && json.itemid != "minecraft:enchanted_book" && json.displayname != "§fMusic Disc"){
         noWikiLinks.push(fileObjs[i].name);
-        console.log(getName(json.displayname));
     }
 }
 
-cron.schedule('* * * * *', () => {
-    console.log('running a task every minute');
+cron.schedule('* * * * * *', () => {
+    
 });
 
 function getName(displayName){
-    return displayName.slice(2);
+    var indices = [];
+    for(var i=0; i<displayName.length;i++) {
+        if (displayName[i] === "§") indices.push(i);
+    }
+    console.log(indices);
+    for(var i in indices){
+        displayName = displayName.slice(0,indices[i]-2*i) + displayName.slice((indices[i]+2)-2*i);
+        console.log(displayName);
+    }
+    return displayName;
 }
 
 /*var writtenData = JSON.stringify(noWikiLinks, null, 2);
