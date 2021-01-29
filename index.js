79df7216f540006c4524d90d5958298d6d216944
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-01-29 09:06:19 +0100

    started working on code

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const https = require('https');
 const fs = require('fs');
 
+var fileObjs = fs.readdirSync("repo/items", { withFileTypes: true });
+var noWikiLinks = new Array();
+var addedWikiLinks = new Array(); 
+var failedWikiLinks = new Array();  
+for(var i in fileObjs){
+    var rawdata = fs.readFileSync(`repo/items/${fileObjs[i].name}`, function (err){
+        if(err){
+            return;
+        }
+    });
+    var json = JSON.parse(rawdata);
+    if(!json.hasOwnProperty("info") && json.itemid != "minecraft:enchanted_book" && json.displayname != "§fMusic Disc"){
+        noWikiLinks.push(fileObjs[i].name);
+    }
+}
+
+
+function getName(displayName){
+    var loc = displayName.indexOf('§');
+}
+
+/*var writtenData = JSON.stringify(noWikiLinks, null, 2);
+fs.writeFileSync(`data.json`, writtenData, function (err){
+    if (err) {
+        return console.log(err);
+        }
+});*/
