647bc75a778bc142be8a2be7b8dc807c1c106be8
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-01-29 18:14:52 +0100

    made stupid mistakes, I should test before comit

--- get-wiki-links/index.js
+++ get-wiki-links/index.js
@@ -16,7 +16,7 @@ for(var i in fileObjs){
     });
     var json = JSON.parse(rawdata);
     if(!json.hasOwnProperty("info") && json.itemid != "minecraft:enchanted_book" &&  json.itemid != "minecraft:potion"){
-        if(!json.hasOwnProperty(vanilla)){
+        if(!json.hasOwnProperty("vanilla")){
             noWikiLinks.push(fileObjs[i].name);
         }else if(!json.vanilla){
             noWikiLinks.push(fileObjs[i].name);
